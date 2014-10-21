144d66cfed9be6db6f0545f62b63a366f194a8bd
coderfengyun <chentienan@yeah.net>
2014-10-21 16:06:45 +0800

    refactor

--- mapreduce/src/main/java/org/tcse/mapreduce/Main.java
+++ mapreduce/src/main/java/org/tcse/mapreduce/Main.java
@@ -17,29 +17,30 @@ import org.tcse.mapreduce.MatrixMultiply.Reduce;
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
-		String pathNameOfA = "hdfs://133.133.134.188:9000/electric-experiment/lab_bigmmult_a.txt";
-		String pathNameOfB = "hdfs://133.133.134.188:9000/electric-experiment/lab_bigmmult_b.txt";
-		String output = "hdfs://133.133.134.188:9000/electric-experiment/lab_bigmmult_c.txt";
+		JobConf conf = createConf();
+		String fsDefaultName = conf.get("fs.default.name");
+		String pathNameOfA = fsDefaultName + "/electric-experiment/lab_bigmmult_a.txt";
+		String pathNameOfB = fsDefaultName + "/electric-experiment/lab_bigmmult_b.txt";
+		String result = fsDefaultName + "/electric-experiment/lab_bigmmult_c.txt";
+		FileInputFormat.setInputPaths(conf, new Path(pathNameOfA), new Path(pathNameOfB));
+		FileOutputFormat.setOutputPath(conf, new Path(result));
+		JobClient.runJob(conf);
+		System.exit(0);
+	}
 
+	private static JobConf createConf() {
 		JobConf conf = new JobConf(MatrixMultiply.class);
 		conf.setJobName("WordCount");
 		conf.addResource("classpath:/hadoop/core-site.xml");
 		conf.addResource("classpath:/hadoop/hdfs-site.xml");
 		conf.addResource("classpath:/hadoop/mapred-site.xml");
-
 		conf.setOutputKeyClass(Text.class);
 		conf.setOutputValueClass(Text.class);
-
 		conf.setMapperClass(Map.class);
 		conf.setCombinerClass(Reduce.class);
 		conf.setReducerClass(Reduce.class);
-
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
-
-		FileInputFormat.setInputPaths(conf, new Path(pathNameOfA), new Path(pathNameOfB));
-		FileOutputFormat.setOutputPath(conf, new Path(output));
-		JobClient.runJob(conf);
-		System.exit(0);
+		return conf;
 	}
 }
